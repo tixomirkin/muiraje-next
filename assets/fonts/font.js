cecea9ef1904eca2b33f26cbaa0b08ed9829cc40
tixomirkin <tixomir5150@gmail.com>
2024-09-28 15:50:20 +0300

    init9

---
+++ assets/fonts/font.js
@@ -0,0 +1,16 @@
+import localFont from "next/font/local";
+
+export const basis33 = localFont({
+    src: "./basis33.woff2",
+    variable: "--font-basis33"
+})
+
+export const swissSiena = localFont({
+    src: "./swiss_siena_rus_by_daymarius.woff2",
+    variable: "--font-swiss-siena"
+})
+
+export const ZTFormom = localFont({
+    src: "./ZT_Formom.woff2",
+    variable: "--font-zt-formom"
+})
